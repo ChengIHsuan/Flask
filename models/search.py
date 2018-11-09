127adca2cc152bb011639f80f0a162475c7c1500
ChengIHsuan <cosine12621@gmail.com>
2018-11-09 11:05:01 +0800

    1109_將結果寫進表格

--- models/search.py
+++ models/search.py
@@ -1,4 +1,6 @@
 import sqlite3
+from flask import Flask, request, render_template
+import numpy as np
 from pandas import DataFrame
 
 class Hospital():
@@ -26,11 +28,27 @@ class Hospital():
         sqlstr = "SELECT id, name, type, address FROM hospitals WHERE address LIKE '" + area + "'"
         results = self.cursor.execute(sqlstr).fetchall()  ##執行sqlstr，並列出所有結果到results
 
-        ##results是list，將results轉成字串res
-        res = ''
-        for i in range(len(results)):
-            res += (str(results[i]) + '<br>')  ##<br>：換行符號
-        ##回傳字串res
-        return res
+        x = np.array(results, dtype=str)
+        context = []
+        amount = len(results)
 
+        # ##results是list，將results轉成字串res
+        # res = ''
+        # for i in range(len(results)):
+        #     res += (str(results[i]) + '<br>')  ##<br>：換行符號
+        # ##回傳字串res
+        return Hospital().table(context=context, x=x, amount=amount)
+
+    def table(self, context, x, amount):
+        # sqlstr = "SELECT id, name, type, address FROM hospitals WHERE id <30"
+        # results = self.cursor.execute(sqlstr).fetchall()
+
+        for i in range(amount):
+            context.append({
+                    'id':(x[i,0]),
+                    'name':(x[i,1]),
+                    'type': (x[i, 2]),
+                    'address': (x[i, 3]),
+                })
+        return render_template('testface.html', context = context)
         # return '<p>id：%s   name：%s</p>' % (hos_id, hos_name)
