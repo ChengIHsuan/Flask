ace4b4add4c7baced30aa37f10aedbe040713d5f
ChengIHsuan <cosine12621@gmail.com>
2018-11-11 16:49:29 +0800

    1111_searchType

--- app.py
+++ app.py
@@ -19,18 +19,35 @@ def index():
     return '<h1>Hello World</h1>'
 
 ##在地區搜尋介面取得使用者輸入的值/search_area
-@app.route('/search_area', methods=['GET'])
+@app.route('/search', methods=['GET'])
 def render():
     return render_template('searchArea.html')
 
 ##顯示搜尋結果/search_area
-@app.route('/search_area', methods=['POST'])
-def searchArea():
-    ##從前端searchArea.html的unputbox的name抓使用者輸入的值
-    county = request.form.get("county")
-    township = request.form.get("township")
-    ##使用class Hospital()裡面的search_area方法
-    return Search().search_area(county, township)
+# @app.route('/search_area', methods=['POST'])
+# def searchArea():
+#     ##從前端searchArea.html的unputbox的name抓使用者輸入的值
+#     county = request.form.get("county")
+#     township = request.form.get("township")
+#     ##使用class Hospital()裡面的search_area方法
+#     return Search().search_area(county, township)
+
+@app.route('/search', methods=['POST'])
+def panduan():
+    if request.method == 'POST':
+        if 'searchArea' in request.form:
+            #從前端searchArea.html的unputbox的name抓使用者輸入的值
+            county = request.form.get("county")
+            township = request.form.get("township")
+            ##使用class Hospital()裡面的search_area方法
+            return Search().search_area(county, township)
+        elif 'searchDisease' in request.form:
+            disease = request.form.get('disease')
+            return 'searchDisease'
+        elif 'searchType' in request.form:
+            type = request.form.get('type')
+            return Search().search_type(type)
+
 
 ##啟動
 if __name__ == '__main__':
--- models/search.py
+++ models/search.py
@@ -1,6 +1,7 @@
 import sqlite3
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash
 import numpy as np
+from tkinter import messagebox
 
 class Search():
 
@@ -10,9 +11,35 @@ class Search():
 
     ##看這個就好
     def search_area(self, county, township):
-        area = str(county + '%' + township + '%')
-        sqlstr = "SELECT id,name,type,address FROM hospitals WHERE address LIKE '" + area + "'"
+        ##若使用者輸入臺北，將會出現臺北及新北資料
+        i = county.find('臺北')
+        if i != -1:
+            area = str("_北%" + township)
+        else:
+            area = str(county + '%' + township)
+        sqlstr = "SELECT id,name,type,address FROM hospitals WHERE address LIKE '" + area + "%'"
         results = self.cursor.execute(sqlstr).fetchall()  ##執行sqlstr，並列出所有結果到results[]
+
+        if results == []:
+            return "none"
+        else:
+            ##將二維陣列results[]轉成numpy array，並計算總資料數
+            n = np.array(results, dtype=str)
+            amount = len(results)
+            ##取得sqlstr中select的欄位，並以","做分割
+            s = sqlstr.index("FROM") - 1
+            column = (sqlstr[7:s]).split(',')
+            return Search().table(n, amount, column)
+
+    def search_type(self, type):
+        t = {
+            '1': "醫學中心",
+            '2': "區域醫院",
+            '3': "地區醫院",
+            '4': "診所"
+        }
+        sqlstr = "SELECT id,name,type,address FROM hospitals WHERE type = '" + t.get(type) + "'"
+        results = self.cursor.execute(sqlstr).fetchall()
 
         ##將二維陣列results[]轉成numpy array，並計算總資料數
         n = np.array(results, dtype=str)
@@ -24,12 +51,12 @@ class Search():
 
     ##將搜尋結果寫進表格，需傳入(numpy array,總資料數,欄位名稱)
     def table(self, n, amount, column):
+        ##建立一個陣列，儲存整理好的資料結果
         context = []
         for i in range(amount):
-            context.append({
-                    column[0]: (n[i, 0]),
-                    column[1]: (n[i, 1]),
-                    column[2]: (n[i, 2]),
-                    column[3]: (n[i, 3]),
-                })
-        return render_template('testface.html', context = context)
+            ##建立一個dict，獎每一筆結果轉存成dict的型態
+            d = {}
+            for j in range(len(column)):
+                d[column[j]] = n[i,j]
+            context.append(d)
+        return render_template('searchArea.html', context = context)
