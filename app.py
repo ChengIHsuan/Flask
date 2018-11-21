f55a6d65577581f3d82f1f71663a7f92029f68cc
ChengIHsuan <cosine12621@gmail.com>
2018-11-21 10:19:39 +0800

    1121_手機搜尋

--- app.py
+++ app.py
@@ -59,19 +59,74 @@ def panduan():
 
 @app.route('/sort', methods=['GET'])
 def renderSort():
-    return render_template('Sort.html')
+    return render_template('sort.html')
 
 @app.route('/collection', methods=['GET'])
 def renderCollection():
-    return render_template('Collection.html')
+    return render_template('collection.html')
 
 @app.route('/login', methods=['GET'])
 def renderLogin():
-    return render_template('Login.html')
+    return render_template('login.html')
 
 @app.route('/signup', methods=['GET'])
 def renderSignup():
-    return render_template('Signup.html')
+    return render_template('signup.html')
+
+##########手機版##########
+@app.route('/phone_search', methods=['GET'])
+def renderPhoneSearch():
+    return render_template('phoneHospital.html')
+
+@app.route('/phone_search', methods=['POST'])
+def phone_panduan():
+    if request.method == 'POST':
+        if 'searchArea' in request.form:
+            #從前端searchArea.html的unputbox的name抓使用者輸入的值
+            county = request.form.get("county")
+            township = request.form.get("township")
+            ##使用class Hospital()裡面的search_area方法
+            return Search().search_area(county, township)
+        elif 'searchDisease' in request.form:
+            disease = request.form.get('disease')
+            return Search().search_disease(disease)
+        elif 'searchType' in request.form:
+            type = request.form.get('type')
+            return Search().search_type(type)
+        elif 'searchCategory' in request.form:
+            keyword1 = request.form.get('keyword1')
+            keyword2 = request.form.get('keyword2')
+            keyword3 = request.form.get('keyword3')
+            keywords = []
+            keywords.append(keyword1)
+            keywords.append(keyword2)
+            keywords.append(keyword3)
+            return Search().search_category(keywords)
+        elif 'searchName' in request.form:
+            name1 = request.form.get('name1')
+            name2 = request.form.get('name2')
+            name3 = request.form.get('name3')
+            names = []
+            names.append(name1)
+            names.append(name2)
+            names.append(name3)
+            return Search().search_name(names)
+
+@app.route('/phone_sort', methods=['GET'])
+def renderPhoneSort():
+    return render_template('phoneSort.html')
+
+@app.route('/phone_collection', methods=['GET'])
+def renderPhoneCollection():
+    return render_template('phoneCollection.html')
+
+@app.route('/phone_login', methods=['GET'])
+def renderPhoneLogin():
+    return render_template('phoneLogin.html')
+
+@app.route('/phone_signup', methods=['GET'])
+def renderPhoneSignup():
+    return render_template('phoneSignup.html')
 
 ##啟動
 if __name__ == '__main__':
--- models/search.py
+++ models/search.py
@@ -10,8 +10,6 @@ class Result():
 
     ##欄位名稱
     def get_column_name(self, results, sqlstr):
-        ##將二維陣列results[]轉成numpy array，並計算總資料數
-        n = np.array(results, dtype=str)
         amount = len(results)
         ##取得sqlstr中select的欄位，並以","做分割
         s = sqlstr.index("FROM") - 1
@@ -19,22 +17,22 @@ class Result():
         ##取得欄位名稱
         columns = []
         for c in getColumns:
-            col = self.cursor.execute("SELECT chi_name FROM column_name WHERE name = '" + c + "'").fetchone()[0]
+            col = self.cursor.execute("SELECT abbreviation FROM column_name WHERE name = '" + c + "'").fetchone()[0]
             columns.append(col)
-        return Result().table(n, amount, columns)
+        return Result().table(results, amount, columns)
 
     ##將搜尋結果寫進表格，需傳入(numpy array,總資料數,欄位名稱)
-    def table(self, n, amount, columns):
+    def table(self, results, amount, columns):
         ##建立一個陣列，儲存整理好的資料結果
         context = []
-        for i in range(amount):
+        for i in range(len(results)):
             ##建立一個dict，獎每一筆結果轉存成dict的型態
             d = {}
             for j in range(len(columns)):
-                d[columns[j]] = n[i, j]
+                d[columns[j]] = results[i][j]
             context.append(d)
         long = len(columns)
-        return render_template('hospital.html', scroll= 'results' ,context=context, columns=columns, long=long)
+        return render_template('searchArea.html', scroll= 'results' ,context=context, columns=columns, long=long)
 
 
 class Search():
@@ -63,25 +61,66 @@ class Search():
 
     ##特殊疾病搜尋
     def search_disease(self, disease):
-        try:
-            getId = self.cursor.execute("SELECT id FROM diseases WHERE (name LIKE '%" + disease + "%' OR eng_name LIKE '%" + disease + "%')")
-            diseaseId = (getId.fetchone()[0])
-            str = {
-                1: "f.h_1, f.h_2, f.h_3, f.h_4, f.h_5",
-                2: "f.h_6, f.h_7, f.h_8, f.h_9, f.h_10, f.h_11",
-                3: "f.h_12, f.h_13, f.h_14, f.h_15",
-                4: "f.h_16, f.h_17, f.h_18",
-                5: "f.h_19, f.h_20, f.h_21, f.h_22",
-                6: "f.h_23, f.h_24, f.h_25, f.h_26, f.h_27",
-                7: "f.h_28, f.h_29, f.h_30, f.h_31",
-                8: "f.h_32, f.h_33"  ##substr
-            }
-            sqlstr = "SELECT h.name, " + str.get(diseaseId) + " FROM hospitals h JOIN final_data f ON h.id = f.hospital_id"
-            results = self.cursor.execute(sqlstr).fetchall()  ##執行sqlstr，並列出所有結果到results[]
-            return Result().get_column_name(results, sqlstr)
-        except:
-            flash('抱歉，找不到您要的「{}」相關資訊。目前只有8個疾病相關的資訊，包括：氣喘疾病(Asthma)、急性心肌梗塞疾病(AMI)、糖尿病(DM，Diabetes)、人工膝關節手術(TKR，Total Knee Replace)、腦中風(Stroke)、鼻竇炎(Sinusitis)、子宮肌瘤手術(Myoma)、消化性潰瘍疾病(Ulcer)。'.format(disease))
-            return render_template("hospital.html")
+        ##判斷使用者輸入的疾病ID
+        getId = self.cursor.execute("SELECT id FROM diseases WHERE (name LIKE '%" + disease + "%' OR eng_name LIKE '%" + disease + "%')")
+        diseaseId = (getId.fetchone()[0])
+
+        index_range = {
+            1: range(1, 6),
+            2: range(6, 12),
+            3: range(12, 16),
+            4: range(16, 19),
+            5: range(19, 23),
+            6: range(23, 28),
+            7: range(28, 32),
+            8: range(32, 34)
+        }
+
+        sqlstr = "SELECT id, name FROM hospitals"
+        results = self.cursor.execute(sqlstr).fetchall()  ##執行sqlstr，並列出所有結果到results[]
+        amount = len(results)
+        length = 2
+        for index in index_range.get(diseaseId):
+            length += 1
+            sql = "select hospital_id, value || ' (分母：' || denominator || ')' from data where index_id = " + str(index)
+            deno = self.cursor.execute(sql).fetchall()
+            for j in range(0, amount):  ##7134
+                for i in range(0, len(deno)):  ##800
+                    if results[j][0] == deno[i][0]:
+                        results[j] += (deno[i][1],)
+                if len(results[j]) < length:
+                    results[j] += "無相關資料",
+
+        ##取得欄位名稱
+        columns = []
+        columns.append("醫院ID")
+        columns.append("醫院名稱")
+        for r in index_range.get(diseaseId):
+            col = self.cursor.execute("SELECT abbreviation FROM indexes WHERE id = " + str(r)).fetchone()[0]
+            columns.append(col)
+
+        return Result().table(results, amount, columns)
+
+        # try:
+        #     getId = self.cursor.execute("SELECT id FROM diseases WHERE (name LIKE '%" + disease + "%' OR eng_name LIKE '%" + disease + "%')")
+        #     diseaseId = (getId.fetchone()[0])
+        #     str = {
+        #         1: "f.h_1, f.h_2, f.h_3, f.h_4, f.h_5",
+        #         2: "f.h_6, f.h_7, f.h_8, f.h_9, f.h_10, f.h_11",
+        #         3: "f.h_12, f.h_13, f.h_14, f.h_15",
+        #         4: "f.h_16, f.h_17, f.h_18",
+        #         5: "f.h_19, f.h_20, f.h_21, f.h_22",
+        #         6: "f.h_23, f.h_24, f.h_25, f.h_26, f.h_27",
+        #         7: "f.h_28, f.h_29, f.h_30, f.h_31",
+        #         8: "f.h_32, f.h_33"  ##substr
+        #     }
+        #     sqlstr = "SELECT h.id, h.name, " + str.get(diseaseId) + " FROM hospitals h JOIN final_data f ON h.id = f.hospital_id"
+        #     # sqlstr = "SELECT h.id, h.name FROM hospitals h JOIN final_data f ON h.id = f.hospital_id"
+        #     results = self.cursor.execute(sqlstr).fetchall()  ##執行sqlstr，並列出所有結果到results[]
+        #     return Result().get_column_name(results, sqlstr)
+        # except:
+        #     # flash('抱歉，找不到您要的「{}」相關資訊。目前只有8個疾病相關的資訊，包括：氣喘疾病(Asthma)、急性心肌梗塞疾病(AMI)、糖尿病(DM，Diabetes)、人工膝關節手術(TKR，Total Knee Replace)、腦中風(Stroke)、鼻竇炎(Sinusitis)、子宮肌瘤手術(Myoma)、消化性潰瘍疾病(Ulcer)。'.format(disease))
+        #     return render_template("searchArea.html")
 
     ##醫院層級搜尋
     def search_type(self, type):
@@ -98,7 +137,6 @@ class Search():
 
     ##分類主題搜尋
     def search_category(self, keywords):
-
         try:
             substr = ''
             str = {
